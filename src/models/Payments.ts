9459957b8fefc221ecabe46a27d11f42855c0771
Guillermo Tapia <guillermotd23@gmail.com>
2025-09-27 18:56:17 -0400

    cambios menores

---
+++ src/models/Payments.ts
@@ -0,0 +1,44 @@
+import mongoose from "mongoose";
+
+const paymentSchema = new mongoose.Schema(
+  {
+    user: {
+      // Reference to the user who made the payment
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "UserModel",
+    },
+    reference: {
+      // Unique payment reference
+      type: String,
+      required: true,
+    },
+    currency: {
+      type: String,
+      required: true,
+    },
+    status: {
+      type: String,
+      default: "pending",
+      required: true,
+    },
+    susbcriptionPlan: {
+      // Subscription plan chosen by the user
+      type: String,
+      required: true,
+    },
+    amount: {
+      type: Number,
+      default: 0,
+    },
+    monthlyRequestCount: {
+      // Monthly API request limit based on subscription type
+      type: Number,
+      default: 0,
+    },
+  },
+  { timestamps: true }
+);
+
+const paymentModel = mongoose.model("Payments", paymentSchema);
+
+export default paymentModel;
